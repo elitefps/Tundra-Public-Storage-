49d37dada2065d047f4b9801ed82b50d66194eb9
elitefps <141988797+elitefps@users.noreply.github.com>
2024-11-19 19:26:16 +0100

    Update include.lua

--- Misc/include.lua
+++ Misc/include.lua
@@ -1,2 +1,32 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/elitefps/Tundra-Public-Storage-/refs/heads/main/utilities/math.lua",true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/elitefps/Tundra-Public-Storage-/refs/heads/main/Misc/syn%20requests.lua",true))()
+
+
+local function LS(serviceName)
+    local success, service = pcall(function()
+        return game:GetService(serviceName)
+    end)
+    if success then
+        return service
+    else
+        return;
+    end
+end
+
+local services = {
+    "Players", "HttpService", "RunService", "RbxAnalyticsService", "Lighting",
+    "MaterialService", "NetworkClient", "ReplicatedFirst", "ReplicatedStorage",
+    "ServerScriptService", "ServerStorage", "StarterGui", "StarterPack",
+    "Workspace", "Debris", "UserInputService", "TeleportService"
+}
+
+local lodeds = {}
+for _, serviceName in ipairs(services) do
+    lodeds[serviceName] = LS(serviceName)
+end
+
+for serviceName, service in pairs(lodeds) do
+    if service then
+        print("[.sys] | [>]", serviceName, "✓")
+    end
+end
