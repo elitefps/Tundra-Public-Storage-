d2b5c33d3657ab2b77cfae708901bab07f173724
elitefps <141988797+elitefps@users.noreply.github.com>
2024-11-19 18:15:58 +0100

    Update include.lua

---
+++ Misc/include.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/elitefps/Tundra-Public-Storage-/refs/heads/main/utilities/math.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/elitefps/Tundra-Public-Storage-/refs/heads/main/Misc/syn%20requests.lua",true))()
